11ec2926a69697aff0506092e41d9599bde91fd6
Tim Slager <Timtendo12@gmail.com>
2022-01-13 13:47:41 +0100

    Added discord logger + getlog command for crewmembers

---
+++ src/Commands/getlog.js
@@ -0,0 +1,93 @@
+const Command = require("../Structures/command");
+const config = require("../data/config.json");
+
+const PastebinAPI = require('pastebin-js'),
+    pastebin = new PastebinAPI({
+        'api_dev_key' : `${config.PB_DEV}`,
+        'api_user_name' : `${config.PB_USER}`,
+        'api_user_password' : `${config.PB_PASS}`
+    });
+const moment = require("moment");
+const {MessageEmbed} = require("discord.js");
+
+module.exports = new Command({
+    name: `getlog`,
+    descripton: `Request logs of today.`,
+    async run(message, args, client) {
+        const definedLogFile = args.slice(1).join(" ");
+        const verifyServer = client.guilds.cache.get(config.serverId);
+        const member = await verifyServer.members.fetch(message.author.id);
+
+        //dir
+        const D = `../AquiverDiscord-Bot/src/data/discord-logger/${moment().format(`YYYY`)}/${moment().format(`MMM`)}/${moment().format(`Do`)}`;
+        const DGen = `${D}/general.txt`
+        const DBot = `${D}/botcmd.txt`
+        const DMedia = `${D}/media.txt`
+
+        //pastebin variables
+        let dir;
+        let title;
+        let ch;
+        let date;
+
+        function checkRequestedLog(){
+            if (definedLogFile.toLowerCase() === "general"){
+                //General.txt is requested
+                dir = DGen;
+                title = `GEN_${moment().format(`YYYY`)}_${moment().format(`MMM`)}_${moment().format(`Do`)}`
+                ch = "General"
+                date = `${moment().format(`YYYY`)}_${moment().format(`MMM`)}_${moment().format(`Do`)}`
+            } else if (definedLogFile.toLowerCase() === "botcommands" || definedLogFile.toLowerCase() === "bot" || definedLogFile.toLowerCase() === "botcmds"){
+                //Botcmd.txt is requested
+                dir = DBot;
+                title = `BOT_${moment().format(`YYYY`)}_${moment().format(`MMM`)}_${moment().format(`Do`)}`
+                ch = "bot-cmd"
+                date = `${moment().format(`YYYY`)}_${moment().format(`MMM`)}_${moment().format(`Do`)}`
+            } else if (definedLogFile.toLowerCase() === "media" || definedLogFile.toLowerCase() === "med"){
+                //media.txt is requested
+                dir = DMedia;
+                title = `MED_${moment().format(`YYYY`)}_${moment().format(`MMM`)}_${moment().format(`Do`)}`
+                ch = "Media"
+                date = `${moment().format(`YYYY`)}_${moment().format(`MMM`)}_${moment().format(`Do`)}`
+            }
+        }
+
+        if (!member.roles.cache.has(`${config.crewID}`)) return;
+        if(message.channel.id !== config.CrewChatID) return;
+        if (!definedLogFile){
+            const noDefinedLog = new MessageEmbed()
+                .setTitle("Please tell me which log file you want to view")
+                .setDescription(`Usage: ${config.prefix}getlog general/botcommands/media`)
+                .setAuthor(`${client.user.tag}`, `${client.user.displayAvatarURL()}`, `https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley`)
+            message.reply({embeds: [noDefinedLog]});
+            return;
+        }
+        checkRequestedLog();
+        pastebin.createPasteFromFile({
+                filename: `${dir}`,
+                title: `${title}`,
+                format: null,
+                privacy: 1,
+                expiration: 'N'
+            }).then(function (data) {
+            //-----------------------------------------------
+            //Embed message which gets sent in the logChannel.
+            //-----------------------------------------------
+            const pbAPILogEmbed = new MessageEmbed()
+                .setTitle(`${title}`)
+                .setURL(`${data}`)
+                .setColor('AQUA')
+                .setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL()}`, `${data}`)
+                .setTimestamp()
+                .addFields(
+                    { name: 'Channel:', value: ch? ch : `No reason specified`, inline: true },
+                    { name: `Date:`, value: date? date : "No date specified", inline: true}
+                )
+            console.log(data);
+            message.reply({embeds: [pbAPILogEmbed]});
+        })
+            .fail(function (err) {
+                console.log(err);
+            });
+    }
+});
--- src/Events/discord-logger.js
+++ src/Events/discord-logger.js
@@ -1,15 +1,4 @@
-/*
-TODO:
 
->  automatically create a fs. PH: year/month/day >> year folder, month folder, day folder.
-
->   log the messages using a message Collector
-
->   create a command that allows staff to request a pastebin link for the requested channel. (Date has to be specified)
-
- */
-
-/*-------------------------------------------------------------*/
 const config = require("../data/config.json");
 const Event = require("../Structures/Event.js");
 const moment = require("moment");
@@ -19,25 +8,116 @@ const fs = require("fs");
 
 
 // module ready. The next code gets executed when the bot starts.
-module.exports = new Event("ready", (client) => {
+module.exports = new Event("ready", (client, message) => {
     //1. Check if the folder for today already exist.
 
     //dir = ../data/discord-logger/YEAR/MONTH/DAY
 
     const dir = `../AquiverDiscord-Bot/src/data/discord-logger/${moment().format(`YYYY`)}/${moment().format(`MMM`)}/${moment().format(`Do`)}`;
-    console.log(dir);
-    discordLogger();
-    function discordLogger() {
+    const generalDir = `${dir}/general.txt`;
+    const generalCh = client.channels.cache.get(`${config.GeneralID}`);
+    const botcmdDir = `${dir}/botcmd.txt`;
+    const botchannel = client.channels.cache.get(`${config.botChannelID}`);
+    const mediaDir = `${dir}/media.txt`;
+    const mediaCh = client.channels.cache.get(`${config.MediaID}`);
+    const firstLine = `-={Aquiver Discord Logger}=- Made by: Timtendo12 ~ date: ${moment().format('dddd')}, ${moment().format('LL')}`;
+
+    const generalCollector = generalCh.createMessageCollector({maxProcessed: 15000});
+    const botcmdCollector = botchannel.createMessageCollector({maxProcessed: 15000});
+    const mediaCollector = mediaCh.createMessageCollector({maxProcessed: 15000});
+
+    checkDateDirExist();
+    //checks the date every 5 minutes. Lets hope this does not take much CPU or RAM.
+    setInterval(checkDateDirExist, 300*1000);
+
+    function checkDateDirExist() {
         //checks if the dir does not exist
         if (!fs.existsSync(dir)) {
             //it does not ~ creates the dir
-            console.log("Directory did not exist yet, Creating one right now.")
+            //console.log("Directory did not exist yet, Creating one right now.")
             fs.mkdirSync(dir, {
                 recursive: true
             });
+            checkIfTXTExists();
         } else {
             //Dir exist ~ continues
-            console.log("Directory Exists!");
+            //console.log("Directory Exists!");
+            checkIfTXTExists();
         }
+    }
+
+    function checkIfTXTExists() {
+        const dirs = [generalDir, mediaDir, botcmdDir];
+        const dirArrayLength = dirs.length;
+        for (let i = 0; i < dirArrayLength; i++) {
+            if (!fs.existsSync(dirs[i])) {
+                fs.appendFile(dirs[i], firstLine, function (err) {
+                    if (err) throw err;
+                    console.log(`Created ${dirs[i]}`);
+                    //genLogger();
+                })
+            } else {
+                //console.log(`${dirs[i]} already exist`);
+            }
+        }
+    }
+    //data used: collector, dir
+    const Dir2 = [generalDir, botcmdDir, mediaDir];
+    const CHloggers = [generalCollector, botcmdCollector, mediaCollector];
+
+    //variables for collector
+    let collector;
+    let dir2;
+    let BotImageLink;
+    let sender = "USER";
+
+    CHArray();
+    function CHArray() {
+        const CHLoggersAL = CHloggers.length;
+        for (let k = 0; k < CHLoggersAL; k++) {
+            collector = CHloggers[k];
+            collectorOn();
+        }
+    }
+
+    function checkAttach(message) {
+        if (message.attachments.size > 0) {
+            message.attachments.forEach(attachment => {
+                BotImageLink = `[${attachment.proxyURL}]`;
+            })
+        }
+    }
+
+    function checkSender(message) {
+        if (message.author.bot) {
+            sender = "BOT";
+        }
+    }
+
+    function checkChannel(message){
+        if (message.channel.id === config.botChannelID){
+            dir2 = botcmdDir;
+        } else if (message.channel.id === config.GeneralID){
+            dir2 = generalDir;
+        } else if (message.channel.id === config.MediaID){
+            dir2 = mediaDir;
+        }
+    }
+
+    function collectorOn() {
+        collector.on(`collect`, message => {
+            BotImageLink = ""
+            checkAttach(message);
+            checkSender(message);
+            checkChannel(message)
+            fs.appendFile(dir2, `\n[${moment().format('LTS')} ${sender}]: [${message.author.tag}] ${message.content} ${BotImageLink}`, err => {
+                if (err) {
+                    console.log(`Failed to log the message with attachment sent by ${sender} ${message.author.tag}`);
+                    console.log(err);
+                    return;
+                }
+                console.log(dir2)
+            });
+        })
     }
 });
